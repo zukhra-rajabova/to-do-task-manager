d308a13fd955e54a24d043d4242b5e35b32110b8
Zukhra Rajabova <zukhrarajabova18@gmail.com>
2026-04-22 22:49:05 +0200

    updated add_task feature

--- add_task.py
+++ add_task.py
@@ -7,8 +7,20 @@ class Task:
         self.due_date = due_date
         self.status = status
 
-
 def add_task(task_list, next_id, title, description, priority, due_date):
+
+    if not isinstance(task_list, list):
+        raise AttributeError("task_list must be a list")
+
+    if not isinstance(next_id, int):
+        raise TypeError("next_id must be integer")
+
+    if not isinstance(title, str):
+        raise TypeError("title must be string")
+
+    if title.strip() == "":
+        raise ValueError("title cannot be empty")
+
     new_task = Task(next_id, title, description, priority, due_date)
     task_list.append(new_task)
     return new_task, next_id + 1
